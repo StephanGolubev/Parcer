fb69529e181b2e7ca753677058fd1ffe6f6998a3
Stephan <golubevstephan@gmail.com>
2019-03-26 17:21:37 +0300

    Все переменные берутся!

--- app/catalog.php
+++ app/catalog.php
@@ -13,12 +13,13 @@ $( document ).ready(function() {
     $("#loading").hide();
 });
 </script>
-<img id="loading" alt="" src="../media/loader.gif"/>
+<!-- <img id="loading" alt="" src="../media/loader.gif"/> -->
 
 
 <?php require "../includes/nava.php" ?>
 <?php
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 600);
+error_reporting(0);
 
 require_once 'classes/db.php';
 require_once 'classes/getter.php';
@@ -41,7 +42,7 @@ echo "</table>";
     $object = new Getter();
     $object->url = "http://дизайн-керамика.рф". $row['url']."";
     $result = $object->get_web_page();
-    // echo htmlspecialchars($result);
+//    print_r($result);
 
     if (($result['errno'] != 0 )||($result['http_code'] != 200)){
         echo "No";
@@ -49,7 +50,6 @@ echo "</table>";
 	}else{
 
     $page2 = $result['content'];
-    // $result.header_remove('content');
     // echo htmlspecialchars($page2);
 
     $pos = strpos($page2, "<!-- Catalog content -->");
@@ -71,7 +71,7 @@ echo "</table>";
     if(preg_match_all("/$regex/iU", $page2, $matches)) {
         foreach ($matches[0] as $key => $value) {
             echo '<a href="http://дизайн-керамика.рф'.$value.'">'.$value.'</a>'."<br>";
-            echo "http://дизайн-керамика.рф".$value."<br><hr>";
+            // echo "http://дизайн-керамика.рф".$value."<br>";
 
             
         $getter = new Getter();
@@ -83,13 +83,55 @@ echo "</table>";
 	}
     else{
         echo "good...";
+        // print_r($getter_result);
         $page_product = $getter_result['content'];
-// echo htmlspecialchars($page_product);
+        // echo ($page_product);
         $page_aft = strpos($page_product, '<p class="the_title">');
         $page_product = substr($page_product, $page_aft);
-        $page_aft = strpos($page_product, '<div class="filters__group__body">');
+        $page_aft = strpos($page_product, '<div class="product__info--other">');
         $page_product = substr($page_product, 0, $page_aft);
-        htmlspecialchars($page_product);
+        // htmlspecialchars($page_product);
+        // echo $page_product;
+        
+        $massiv = array();
+        $reg_ex1 = "<(.|\n)*?>";
+        $reg_ex2 = "<div[^<>]*class=\"[^\"']+\"[^<>]*>[\s\S]*?</div>";
+        $reg_ex = "<div[^<>]*class=\"my-class\"[^<>]*>[\s\S]*?";
+
+
+        // $nodes = $xp->query('//div[@class="product__info__group"]');
+        // print_r($nodes);
+
+
+        $dom = new DomDocument();
+// $dom->load($page_product);
+$dom->loadhtml('<?xml encoding="utf-8" ?>' . $page_product);
+$finder = new DomXPath($dom);
+$classname="product__info__group";
+$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+print_r($nodes);
+echo $nodes->textContent;
+foreach ($nodes as $value10) {
+    echo $value10->textContent."<br>";
+}
+
+        // (?<=href=\").+(?=\")
+        // class="product__info__name">
+
+        // str_replace("'", "\"", $page_product);
+        preg_match_all('~the_title">\K(?:[^<]+)(?=<)~', $page_product, $matches10);
+        print_r($matches10);
+        echo "<hr>";
+    // if(preg_match_all("/class=\"product__info__value\">[.*]</iU", $page_product, $mat)) {
+    //   }
+    //   foreach ($mat[0] as $value1) {
+        //   if ($value1 = 'Страна'or 'Производитель' or 'Код товара') {
+            //   array_push($massiv,$value1);
+            // echo "No<br>";
+        //   }
+        //   echo  "<li>".$value1."<br>";
+    //   }
+    //   print_r($massiv);
     }
 
 }
--- app/classes/getter.php
+++ app/classes/getter.php
@@ -11,12 +11,11 @@ class Getter {
       
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // возвращает веб-страницу
         curl_setopt($ch, CURLOPT_HEADER, 0);           // не возвращает заголовки
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   // переходит по редиректам
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);   // переходит по редиректам
         curl_setopt($ch, CURLOPT_ENCODING, "");    
         curl_setopt($ch, CURLOPT_USERAGENT, "booyah!");    // обрабатывает все кодировки
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); // таймаут соединения
         curl_setopt($ch, CURLOPT_TIMEOUT, 0);        // таймаут ответа
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);       // останавливаться после 10-ого редиректа
       
         $content = curl_exec( $ch );
         $err     = curl_errno( $ch );
